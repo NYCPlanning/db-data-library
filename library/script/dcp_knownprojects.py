f3e0654ddea10c263afc66234723aa7708a048f2
Te Du <tedudevelop@gmail.com>
2022-12-16 21:08:17 +0000

    add dcp_knownprojects

---
+++ library/script/dcp_knownprojects.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+from . import df_to_tempfile
+from dotenv import load_dotenv
+import boto3
+import os
+
+from io import StringIO
+
+# Load environmental variables
+load_dotenv()
+
+
+class Scriptor:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    @property
+    def version(self):
+        return self.config["dataset"]["version"]
+
+    def ingest(self) -> pd.DataFrame:
+        client = boto3.client(
+            "s3",
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+            endpoint_url=os.environ["AWS_S3_ENDPOINT"]
+        )
+        obj = client.get_object(
+            Bucket="edm-private",
+            Key=f"dcp_knownprojects/kpdb_old_hdb_removed_{self.version}.csv",
+        )
+        # convert to a literal string of bytes with correct encoding
+        s = str(obj["Body"].read(), "utf-8")
+        # use StringIO to convert consumable format for pd.read_csv
+        data = StringIO(s)
+        df = pd.read_csv(data, encoding="utf-8")
+        return df
+
+    def runner(self) -> str:
+        df = self.ingest()
+        local_path = df_to_tempfile(df)
+        return local_path
