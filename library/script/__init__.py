0a7c0ec4d74f0afa33f80feb102184063f1f16c2
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-08 13:24:21 -0500

    add get_json_content

--- library/script/__init__.py
+++ library/script/__init__.py
@@ -1,4 +1,6 @@
 import tempfile
+import json
+import requests
 
 import pandas as pd
 
@@ -7,3 +9,14 @@ def df_to_tempfile(df: pd.DataFrame) -> str:
     f = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
     df.to_csv(f, index=False)
     return f.name
+
+
+def get_json_content(url: str) -> str:
+    # use default headers to prevent "403 Client Error: Forbidden for url"
+    default_headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    }
+    response = requests.get(url, headers=default_headers)
+    response.raise_for_status()  # raises exception when not a 2xx response
+    content = json.loads(response.content)
+    return content
