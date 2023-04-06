5456e44fc07bb89a0c7b53f9d614b3b6c883f4f3
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-04-06 18:07:43 +0000

    chk

--- data_flow_diagrams/data_flow_diagrams.py
+++ data_flow_diagrams/data_flow_diagrams.py
@@ -1,17 +1,22 @@
+import os
+
 from diagrams import Diagram, Cluster
+from diagrams.custom import Custom
 from diagrams.onprem import client
 from diagrams.programming import flowchart
 from diagrams.programming import language
 from diagrams.generic.blank import Blank
 from diagrams.generic.database import SQL
 from diagrams.digitalocean import storage
+from diagrams.aws.general import InternetAlt1
 
 from library import config
 
-web = flowchart.OffPageConnectorRight
-manual = client.User
+Socrata = lambda name: Custom(name, './data_flow_diagrams/socrata.jpeg')
 
-source_names = [
+all_sources = [ filename[:-4] for filename in os.listdir('./library/templates')]
+
+ztl_sources = [
     "dcp_commercialoverlay",
     "dcp_limitedheight",
     "dcp_specialpurpose",
@@ -19,26 +24,49 @@ source_names = [
     "dcp_zoningmapamendments",
     "dof_dtm",
     "dcp_zoningdistricts",
-    "dcp_zoningmapindex"
+    "dcp_zoningmapindex",
+]
+
+pluto_sources = [
+    "dcp_commercialoverlay",
+    "dcp_limitedheight",
+    "dcp_specialpurpose",
+    "dcp_specialpurposesubdistricts",
+    "dcp_zoningmapamendments",
+    "dof_dtm",
+    "dcp_zoningdistricts",
+    "dcp_zoningmapindex",
+    "dcp_cdboundaries_wi",
+    "dcp_cb2010_wi",
+    "dcp_ct2010_wi",
+    "dcp_cb2020_wi",
+    "dcp_ct2020_wi",
+    "dcp_school_districts",
+    "dcp_firecompanies",
+    "dcp_policeprecincts",
+    "dcp_councildistricts_wi",
+    "dcp_healthareas",
+    "dcp_healthcenters",
+    "dof_shoreline",
+    "doitt_zipcodeboundaries",
+    "fema_firms2007_100yr",
+    "fema_pfirms2015_100yr",
+    "dpr_greenthumb",
+    "dsny_frequencies",
+    "lpc_historic_districts",
+    "lpc_landmarks",
+    "dcp_colp",
+    "dof_condo",
 ]
 
 overrides = {}
 
 path = lambda name: f"./library/templates/{name}.yml"
 
-def library_archive(source_node):
-    processing_node = language.Python("Processing!")
-    source_node >> processing_node
-    return processing_node
-
-def do(source, source_node):
-    do_node = SQL(source)
-    source_node >> do_node
-    return do_node
-
 class source:
     def __init__(self, name):
-        if source in overrides: return overrides[name]
+        if name in overrides:
+            self.overrides = overrides[name]
         self.name = name
         template = config.Config(path(name)).parsed_rendered_template()
         self.dataset = template["dataset"]
@@ -46,10 +74,25 @@ class source:
         self.destination = self.dataset["destination"]
 
     def source(self):
-        self.last_node = manual(self.name)
+        if "url" in self.source_type: 
+            path = self.source_type["url"]["path"]
+            if path.startswith('http'): self.last_node = InternetAlt1(path.split("//")[1].split("/")[0])
+            elif path.startswith('library'): self.last_node = client.User(path[12:])
+            elif path.startswith('s3'): 
+                s3path = path[5:]
+                split_path = s3path.split('/')
+                self.last_node = storage.Volume(split_path[0] + "\n" + split_path[2])
+            else: self.last_node = client.User(path)
+        elif "local_path" in self.source_type:
+            self.last_node = client.User()
+        elif "script" in self.source_type:
+            self.last_node = language.Python(self.name + '.py')
+        elif "socrata" in self.source_type:
+            self.last_node = Socrata(self.name) ## todo needs some more logic
+        else: raise Exception(f"No found source for {self.name}")
 
     def processing(self):
-        node = language.Python("Processing!")
+        node = language.Bash("standard processing")
         self.last_node >> node
         self.last_node = node
 
@@ -57,16 +100,18 @@ class source:
         node = SQL(self.name)
         self.last_node >> node
         self.last_node = node
+        self.do_node = node
 
-sources = [ source(s) for s in source_names ]
+sources = [ source(s) for s in all_sources ]
 
-with Diagram("ztl"):
+with Diagram(""):
     for s in sources: s.source()
 
     with Cluster("Library Archive"):
         for s in sources: s.processing()
 
     with Cluster("Digital Ocean"):
-        #do = storage.Volume('edm-recipes')
-        for s in sources: s.do()
+        with Cluster("edm-recipes"):
+            #do = storage.Volume('edm-recipes')
+            for s in sources: s.do()
     
