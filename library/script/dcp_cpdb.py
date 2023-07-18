69b3a9d52c5c9827d85894eb2838ca6e3261a968
Dea Bardhoshi <dea.bardhoshi@berkeley.edu>
2023-07-18 16:31:55 -0400

    files

--- library/script/dcp_cpdb.py
+++ library/script/dcp_cpdb.py
@@ -1,13 +1,15 @@
 import os
 import glob
-import pandas as pd
+#import pandas as pd
 
-folder_path = "library/tmp/cpdb_projects.csv"
-output_file = "library/tmp/final_output.csv"
-csv_files = glob.glob(os.path.join(folder_path, "*.csv"))
-combined_data = pd.DataFrame()
-for file in csv_files:
-    df = pd.read_csv(file)
-    combined_data = combined_data.append(df)
+print('Testing out script in YML!')
 
-combined_data.to_csv(output_file, index=False)
+#folder_path = "library/tmp/cpdb_projects.csv"
+#output_file = "library/tmp/final_output.csv"
+#csv_files = glob.glob(os.path.join(folder_path, "*.csv"))
+#combined_data = pd.DataFrame()
+#for file in csv_files:
+ #   df = pd.read_csv(file)
+  #  combined_data = combined_data.append(df)
+
+#combined_data.to_csv(output_file, index=False)
