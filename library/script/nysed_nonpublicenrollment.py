d9bd3f0921937aebbf0e3c2b04460ff5afb45034
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-07-21 10:27:10 -0400

    Update recipe for nysed nonpub enrollment
    The url for the xlsx no longer worked, and it appears that the tabs in
    the xlsx changed. Both issues are fixed here.
    
    If specifying the tabname in pandas.read_excel proves fragile, we could
    just specify the tab number instead.

--- library/script/nysed_nonpublicenrollment.py
+++ library/script/nysed_nonpublicenrollment.py
@@ -7,10 +7,12 @@ from .scriptor import ScriptorInterface
 class Scriptor(ScriptorInterface):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-        
+
     def ingest(self) -> pd.DataFrame:
-        df = pd.read_excel(self.path)
-        return df
+        version_in_xlsx_tab = self.version[2:]  # e.g. "2022-23" -> "22-23"
+        return pd.read_excel(
+            self.path, sheet_name=f"NonPubEnroll_byGrade_{version_in_xlsx_tab}"
+        )
 
     def runner(self) -> str:
         df = self.ingest()
